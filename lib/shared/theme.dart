c579b53e6b08ec3e764e78a6412f1daf87a1761e
embunazzahra <dhauembunazzahra@gmail.com>
2022-02-16 12:40:10 +0700

    initial commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+//Colors
+Color kDarkGreyColor = Color(0xff3F414E);
+Color kLightGreyColor = Color(0xffA1A4B2);
+Color kLightPurpleColor = Color(0xff8E97FD);
+Color kWhiteFontButtonColor = Color(0xffF6F1FB);
+Color kWhiteBackgroundColor = Color(0xffFFFFFF);
+Color kWhiteButtonColor = Color(0xffEBEAEC);
+Color kDarkPurpleColor = Color(0xff7583CA);
+Color kFormColor = Color(0xffF2F3F7);
+Color kSandyBeachColor = Color(0xffFFECCC);
+Color kPurpleBackgroundColor = Color(0xff8E97FD);
